52e08d3c9842dae346feb4e7d6f0768fb2908dca
N238635 <lnickersl@gmail.com>
2022-04-09 12:33:31 +0500

    feat: simple server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,32 @@
-console.log('Hello world!');
+import * as dotenv from 'dotenv';
+import * as express from 'express';
+import * as cors from 'cors';
+dotenv.config();
+
+import sequelize from './db';
+
+const {PORT} = process.env;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({message: 'Hello world'});
+});
+
+const start = async () => {
+  try {
+    sequelize.authenticate();
+    sequelize.sync();
+
+    app.listen(PORT, () => {
+      console.log(`Server started on port: ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
